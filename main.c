bb38b7f7955884c7f1daf5baa6ca5818099296ea
chals24x <84355471+ChalsLapWork@users.noreply.github.com>
2024-11-29 16:50:58 -0600

    debug 29-nov-24 450

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main(void){
   signal(SIGINT,signal_handler);//asocia el manejador de salida del programa
   configPuertos();
   init_queues();
-  usdelay(5000000);
+  usleep(5000000);
   test_display();
 
 
