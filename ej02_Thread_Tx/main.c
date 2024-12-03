89992b146e0e166a58cb9af2bdd0348c9eec5e35
chals24x <84355471+ChalsLapWork@users.noreply.github.com>
2024-12-03 17:54:23 -0600

    debug 3-dic-24 554pm

--- ej02_Thread_Tx/main.c
+++ ej02_Thread_Tx/main.c
@@ -22,12 +22,14 @@ typedef struct {
     _Atomic int head;  // Índice de escritura (hilo padre)
     _Atomic int tail;  // Índice de lectura (hilo hijo)
     pthread_mutex_t mutex;
+    pthread_cond_t cond;
 } circular_buffer_t;
 
 circular_buffer_t buffer = {
     .head = 0,
     .tail = 0,
     .mutex = PTHREAD_MUTEX_INITIALIZER
+    .cond  = PTHREAD_COND_INITIALIZER
 };
 
 // Función que simula la transmisión lenta
