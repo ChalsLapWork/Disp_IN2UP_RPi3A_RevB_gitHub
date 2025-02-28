8f02fb7eac3c4084e03c106d4b4e7007d45b91b7
chalsRemoteServer <webmaster@jelly.com.mx>
2025-02-27 19:46:49 -0600

    debug 27-feb-25 744pm disp_RevE2.28 RUNNING, status: Display Portal Inicio: no Completo,
    debug 27-feb-25 744pm disp_RevE2.28 RUNNING, status: Display Portal Inicio: no Completo,
    seguimos modificando el productor tenia el problema que no aumentaba el buffer2-len vamos a probar este nuevo codigo

--- VFDthread.c
+++ VFDthread.c
@@ -110,6 +110,7 @@ int semaforo;
             case 5:if (len > 0 && buffer2_len + len <= MAX_BUFFER_LEN)
                         estado++;else{estado=30;}break;
             case 6:memcpy(buffer2 + buffer2_len, buffer_circular[out].data, len);
+                   buffer2_len+=len;
                    sem_post(&sem_vacios);//incrementa vacios,
                    printf("Productor: Copió datos al buffer2 (len: %zu, total en buffer2: %zu)\n", len, buffer2_len);
                    out = (out + 1) % NUM_ENTRADAS;
