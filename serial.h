c86a048c36bba3394b5a5026991f434a47e2b96e
chals24x <84355471+ChalsLapWork@users.noreply.github.com>
2025-03-11 08:51:02 -0600

    debug: 11-mar-25 0850am Branch: Feature_RevE2_CommProc, status: Debugging new code,
    debug: 11-mar-25 0850am Branch: Feature_RevE2_CommProc, status: Debugging new code,
    se agrego declaracion de func. de serial que faltaban

--- serial.h
+++ serial.h
@@ -2,3 +2,5 @@ void init_Serial(void);
 void Procesamiento_de_cadena_serProc(char *c);
 void procesarComando(unsigned char len,unsigned char cmd,unsigned char crc);
 void procesarCmd(unsigned char cmd,unsigned char *param);
+void *serial_reader(void *arg);
+void *cons_serial_processor(void *arg);
