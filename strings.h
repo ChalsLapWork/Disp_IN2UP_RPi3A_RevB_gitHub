79bc09e41c1ee6ca1d46d7633b6b4bab519b00bd
chals24x <84355471+ChalsLapWork@users.noreply.github.com>
2025-03-18 14:56:41 -0600

    18-mar.-25 255pm: ver:40.0.3.9 Branch:Feature_RevE5_Cuent_Rech, status: Building Code
    18-mar.-25 255pm: ver:40.0.3.9 Branch:Feature_RevE5_Cuent_Rech, status: Building Code
     problema de sintaxis, para probar la funcion de despliegue de numero ushortint

--- strings.h
+++ strings.h
@@ -9,3 +9,4 @@ void convertir_usint_to_BCD(unsigned char *p,unsigned short int var);
 void suma_BCD(unsigned char dm1,unsigned char m1,unsigned char c1,unsigned char d1,unsigned char u1,unsigned char *p);
 void suma_BCD_digito(unsigned char op,unsigned char n,unsigned char *p);
 unsigned char func_Alge(unsigned char x);
+void Formato_USInt(unsigned char *orig,unsigned char *dest,unsigned char size,unsigned char *n);
