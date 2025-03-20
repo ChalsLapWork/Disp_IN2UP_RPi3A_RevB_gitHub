9bb06df7c6764e14c18a74dea78fb6092d471b1d
chals24x <84355471+ChalsLapWork@users.noreply.github.com>
2025-03-20 12:08:26 -0600

    20-mar.-25 1208am: ver:40.0.3.31 Branch:Feature_RevE6 status:BuildingCode
    20-mar.-25 1208am: ver:40.0.3.31 Branch:Feature_RevE6 status:BuildingCode
    tenemos el eje04 funcionando recibiedo datos masivos pero a 115200 vamos a bajar a 9600, Y ha hacer una prueba con ascii de manera mas masiva

--- ej04_Serial_Rx/main_serial.c
+++ ej04_Serial_Rx/main_serial.c
@@ -39,9 +39,10 @@ int main() {
 
                 // Mostrar cada byte en formato hexadecimal
                 for (int i = 0; i < bytes_received; i++) {
-                    printf("%s%02X %s:: %s%c %s", COLOR_ROJO, buffer[i],COLOR_BLANCO,COLOR_AMARILLO,buffer[i], COLOR_RESET);
-
-                }
+                    printf("%s%02X %s ", COLOR_ROJO, buffer[i], COLOR_RESET);}
+                for (int i = 0; i < bytes_received; i++) {
+                    printf("%s%c%s", COLOR_AMARILLO, buffer[i], COLOR_RESET);}
+                    
                 printf("\n");
             }
         }
