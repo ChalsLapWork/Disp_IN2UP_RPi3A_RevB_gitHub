62a6e417d16983319ca139c2e83b6fb4b9f3bd35
chals24x <84355471+ChalsLapWork@users.noreply.github.com>
2025-03-21 15:43:25 -0600

    21-mar.-25 342pm: ver:40.0.3.38 Branch:Feature_RevE6 status:BuildingCode
    21-mar.-25 342pm: ver:40.0.3.38 Branch:Feature_RevE6 status:BuildingCode
    estamos probando el codigo, que modifoco para que la funcion Procesamiento_de_cadena_serProc solo eejecute los bytes que se indican como parametro en lugar que pregunte indefinidamente.

--- serial.h
+++ serial.h
@@ -1,5 +1,5 @@
 int init_Serial(void);
-void Procesamiento_de_cadena_serProc(char *c);
+void Procesamiento_de_cadena_serProc(char *c,int size);
 void procesarComando(unsigned char len,unsigned char cmd,unsigned char crc);
 void procesarCmd(unsigned char cmd,unsigned char *param);
 void *serial_reader(void *arg);
